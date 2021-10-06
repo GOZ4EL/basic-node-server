c3e16c4af60fb5ba38dfc6d261a79695599c1a97
goz4el <hordemzerado@gmail.com>
2021-10-05 20:09:57 -0400

    Fixing function call errors

--- events/httpsniffer.mjs
+++ events/httpsniffer.mjs
@@ -5,23 +5,23 @@ const timestamp = () => new Date().toISOString();
 
 export function sniffOn(server) {
   server.on("request", (req, res) => {
-    console.log(`${timestamp} request`);
-    console.log(`${timestamp} ${reqToString(req)}`);
+    console.log(`${timestamp()} request`);
+    console.log(`${timestamp()} ${reqToString(req)}`);
   });
 
   server.on("close", (errno) => {
-    console.log(`${timestamp} close errno=${errno}`);
+    console.log(`${timestamp()} close errno=${errno}`);
   });
 
   server.on("checkContinue", (req, res) => {
-    console.log(`${timestamp} checkContinue`);
-    console.log(`${timestamp} ${reqToString(req)}`);
+    console.log(`${timestamp()} checkContinue`);
+    console.log(`${timestamp()} ${reqToString(req)}`);
     res.writeContinue();
   });
 
   server.on("upgrade", (req, socket, head) => {
-    console.log(`${timestamp} upgrade`);
-    console.log(`${timestamp} ${reqToString(req)}`);
+    console.log(`${timestamp()} upgrade`);
+    console.log(`${timestamp()} ${reqToString(req)}`);
   });
 
   server.on("clientError", () => {
